1446b43086a723510bbdc48ba5404e964a6b94f3
Caleb Watson <147353712+CalebW317@users.noreply.github.com>
2024-09-09 14:07:46 -0500

    Added a function to shuffle the order of the slides

--- script.js
+++ script.js
@@ -12,6 +12,16 @@ document.addEventListener("DOMContentLoaded", function() {
     // Clear any existing slides to prevent duplication
     slideshow.innerHTML = '';
 
+    // Shuffle the imageList array
+    function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    shuffle(imageList);
+    
     // Load images into the slideshow
     imageList.forEach(image => {
         const slide = document.createElement('div');
